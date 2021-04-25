e3bd5ecdd30cee3506739c42765b121a5ba94ea7
Henrik Hey <henrikheywork@gmail.com>
2021-04-25 15:08:43 -0400

    feat(networking): added semi functional networking

--- src/backend/index.js
+++ src/backend/index.js
@@ -5,33 +5,18 @@ const io = require('socket.io')(server);
 const INITIAL_STATE = 'rnbqkbnr/1ppppppp/p7/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
 
 const sendInitialBoardState = (socket) => {
-	socket.send(INITIAL_STATE);
+	socket.emit("board-state-change", INITIAL_STATE);
 }
 
-const registerBoardChange = () => {
-
+const handleBoardStateChange = (socket) => {
+	socket.on("board-state-change", (FENString) => {
+		console.log(FENString);
+		socket.emit("board-state-change", FENString);
+	})
 }
 
 io.on('connection', socket => {
-
-	// either with send()
 	sendInitialBoardState(socket);
-
-	// or with emit() and custom event names
-	socket.emit("greetings", "Hey!", { "ms": "jane" }, Buffer.from([4, 3, 3, 1]));
-  
-	// handle the event sent with socket.send()
-	socket.on("message", (data) => {
-	  console.log(data);
-	});
-
-	socket.on("board-state-change", (FENString) => {
-		console.log(FENString);
-	})
-  
-	// handle the event sent with socket.emit()
-	socket.on("salutations", (elem1, elem2, elem3) => {
-	  console.log(elem1, elem2, elem3);
-	});
+	handleBoardStateChange(socket);
 });
 server.listen(8080);
